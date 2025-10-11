65dff630133fb8c6399db48985123b756e419177
papani-sivasai <sivasai.papani01@gmail.com>
2025-10-11 15:14:13 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -59,3 +59,4 @@ app.use("/api/shop/review", shopReviewRouter);
 app.use("/api/common/feature", commonFeatureRouter);
 
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
+
